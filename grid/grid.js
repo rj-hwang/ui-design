6919ea976ea25529290c705bfdd9c35a3c03470b
dragon <rongjihuang@gmail.com>
2013-04-07 08:15:04 +0800

    grid

--- grid/grid.js
+++ grid/grid.js
@@ -1,7 +1,11 @@
 (function($) {
 	var $g = $(".grid");
 	$(":radio[name=configHeaderWrap]").on("click",function(){
-		console.log("radio");
 		$g.find("thead>tr").toggleClass("wrap",this.value=='1');
+	});
+	$(":radio[name=configHeaderCellHAlign]").on("click",function(){
+		console.log(this.value);
+		$g.find("thead>tr").removeClass("halign-left halign-center halign-right halign-justify")
+			.toggleClass(this.value,true);
 	});
 })(jQuery);
