1d63954813b2b0944fd7e53afc0db46c29dfd06d
dragon <rongjihuang@gmail.com>
2013-04-10 01:25:00 +0800

    config fontSize and lineHeight for body

--- grid/grid.js
+++ grid/grid.js
@@ -1,6 +1,20 @@
 (function($) {
 	var $g = $(".grid");
 
+	// 字体大小
+	$(":input[name=configFontSize]").on("change",function(){
+		console.log(this.value);
+		$(this).next().text(this.value);
+		document.body.style.fontSize = this.value + "px";
+	});
+
+	// 行高
+	$(":input[name=configLineHeight]").on("change",function(){
+		console.log(this.value);
+		$(this).next().text(this.value);
+		document.body.style.lineHeight = this.value;
+	});
+
 	// 标题行
 	$(":radio[name=configHeaderWrap]").on("change",function(){
 		console.log(this.value);
