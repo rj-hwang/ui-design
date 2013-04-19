682b0ddce222922d0efaebcac11d1411d654734d
dragon <rongjihuang@gmail.com>
2013-04-20 01:34:49 +0800

    t

--- grid/grid.js
+++ grid/grid.js
@@ -1,4 +1,12 @@
 (function($) {
+	var model = {
+		columns: [
+			{},
+			{resizable: true},
+			{},
+			{resizable: true}
+		]
+	};
 	var $g = $(".grid");
 
 	// 字体大小
@@ -51,5 +59,21 @@
 		$g.find(">table>tbody>tr")
 			.removeClass("top middle bottom")
 			.toggleClass(this.value,true);
+	});
+
+	// 列宽指示
+	$g.on("mousemove",">table>thead>tr>*",function(e){
+		if(this.cellIndex > 0 && model.columns[this.cellIndex-1].resizable){
+			var $t = $(this);
+			var x=e.pageX - $t.offset().left, w=this.offsetWidth;
+			console.log("px=" + e.pageX + ",cx=" + e.clientX  + ",ox=" + e.offsetX + ",ol=" + this.offsetLeft + ",cl=" + this.clientLeft + ",cw=" + this.clientWidth + ",ow=" + this.offsetWidth);
+			console.log("l1=" + $t.offset().left + ",l2=" + $t.position().left + ",x=" + x);
+			if(x<=5){
+				$t.css("cursor","col-resize");
+				console.log(e);
+			}else{
+				$t.css("cursor","default");
+			}
+		}
 	});
 })(jQuery);
